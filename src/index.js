f4674af955f3ef45c00df1b5efa9f3690202a9ea
Jared Meyering <jared.meyering@gmail.com>
2017-07-13 17:03:29 +0000

    only add annotated components

--- src/index.js
+++ src/index.js
@@ -1,29 +1,26 @@
 import { utils as docgenUtils } from 'react-docgen'; // eslint-disable-line
 
-const reactDocs = require('react-docgen'); // eslint-disable-line
-
 export default function (ast, recast) {
-  /**
-   * Find all the currently exported component definitions.
-   */
-  const components = reactDocs.resolver.findAllExportedComponentDefinitions(ast, recast);
-
+  const components = [];
   const types = recast.types.namedTypes;
 
   const exportTagged = (path) => {
+    /**
+     * If an export doesn't have leading comments, we can simply continue
+     */
     const leadingComments = path.value.leadingComments;
     if (!leadingComments) {
       return false;
     }
 
-    // Search for the @component tag in any export and if it is present assume
-    // that we are exporting a valid react component.
+    // Search for the @component tag in any export.
     const isTagged = leadingComments.reduce((acc, comment) => acc === true
       || comment.value.indexOf('@component') !== -1, []);
 
     if (!isTagged) {
       return false;
     }
+
 
     const definitions = docgenUtils.resolveExportDeclaration(path, types);
 
